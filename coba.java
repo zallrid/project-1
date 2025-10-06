e04ef02e0546864c34814a3fdc5d82f75e763195
Jujukaizen <juanmadhy425@gmail.com>
2025-10-06 14:17:59 +0700

    COMMMITTTTT ALGORITMAAAA PENGHITUNGANN UANG GWOIII

--- coba.java
+++ coba.java
@@ -1,7 +1,10 @@
 public class coba {
     public static void main(String[] args) {
-        
         System.out.println("tes tes tes");
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("hasilnya adalah " + c);
     }
     
 }
--- importy.java
+++ importy.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class importy {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println(7);
+        System.out.println("Hello World");
+        int hargaBarang = 10000;
+        System.out.println("Harga barang: " + hargaBarang);
+        System.out.print("Masukkan jumlah barang yang dibeli: ");
+        int jumlahBarang = scanner.nextInt();
+        int totalHarga = hargaBarang * jumlahBarang;
+        System.out.println("Total harga: " + totalHarga);
+        scanner.close();
+
     }
 }
